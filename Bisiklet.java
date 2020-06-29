b962448bda9bde1eebb6a5286eab1a0c03d5ba3d
Selimdr <seldr35@gmail.com>
2020-06-29 21:49:18 +0200

    canim patronum gonderdim

--- Bisiklet.java
+++ Bisiklet.java
@@ -3,12 +3,13 @@ package bisiklet;
 public class Bisiklet {
 
 	public static void main(String[] args) {
-         System.out.println("Bugün ilk gün ve calismaya basladik");
-         System.out.println("Patron bugun dosyalari gönderiyorum");
+         System.out.println("Bugun ilk gun ve calismaya basladik");
+         System.out.println("Patron bugun dosyalari gonderiyorum");
          System.out.println("patron geri donus yapmadi");
 	     System.out.println("ikinci gunumu cok heyecanliyim");
 	     System.out.println("Patron bugun yazdiklarimi begenecek mi?");
 	     // guzel olmus fakat ufak degisiklikler gerekiyor
+	     System.out.println("Sayin patronum simdi nasil olmus");
 	}
 
 }
