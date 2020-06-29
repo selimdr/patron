f00ebbf7827a1ea7c18a8620acd710900dec36c0
selimdr <67345675+selimdr@users.noreply.github.com>
2020-06-29 21:37:18 +0200

    Update Bisiklet.java

--- Bisiklet.java
+++ Bisiklet.java
@@ -3,11 +3,12 @@ package bisiklet;
 public class Bisiklet {
 
 	public static void main(String[] args) {
-         System.out.println("Bugün ilk gün ve calismaya basladik");
-         System.out.println("Patron bugun dosyalari gönderiyorum");
+         System.out.println("BugÃ¼n ilk gÃ¼n ve calismaya basladik");
+         System.out.println("Patron bugun dosyalari gÃ¶nderiyorum");
          System.out.println("patron geri donus yapmadi");
 	     System.out.println("ikinci gunumu cok heyecanliyim");
 	     System.out.println("Patron bugun yazdiklarimi begenecek mi?");
+	     // guzel olmus fakat ufak degisiklikler gerekiyor
 	}
 
 }
